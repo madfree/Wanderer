c08cece7cfd6c7590e177cbd547fde6f91cf47ee
madfree <matzefrei@gmail.com>
2020-09-05 14:11:09 +0200

    refactor: Use latitude and longitude as well as marker for new home position

--- app/src/main/java/com/madfree/wanderer/MapsActivity.kt
+++ app/src/main/java/com/madfree/wanderer/MapsActivity.kt
@@ -37,10 +37,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = 47.972959
+        val longitude = 11.351387
+
+        val homeLatLong = LatLng(latitude, longitude)
+        val zoomLevel = 15f
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLong, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLong))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
